76856b2f439e4cdd11d578cd79f6831c0b5d4a2c
Yusuf <yumustyologt@example.com>
2023-04-05 16:01:49 +0100

    updates

--- src/utils/orderIdGenerator.ts
+++ src/utils/orderIdGenerator.ts
@@ -6,15 +6,17 @@ export async function generateOrderId() {
   const lastOrderId = (await getLastOrderIdFromDatabase()) || "RX:000000";
 
   // Increment the numeric portion of the last order ID by 1
-  const lastOrderNumber = parseInt(lastOrderId.substring(3));
+
+  const lastOrderPrefix = lastOrderId.split(":")[0];
+  const lastOrderNumber = parseInt(lastOrderId.split(":")[1]);
   const nextOrderNumber = lastOrderNumber + 1;
 
   // Check if the numeric portion of the order ID is about to exceed six digits
   if (nextOrderNumber >= 1000000) {
     // Switch to a new prefix for the order ID
-    const newPrefix = generateNewPrefix();
+    const newPrefix = await generateNewPrefix(lastOrderPrefix);
     const newOrderId = `${newPrefix}:000001`;
-    //   await saveOrderIdToDatabase(newOrderId);
+
     return newOrderId;
   }
 
@@ -22,60 +24,58 @@ export async function generateOrderId() {
   const paddedOrderNumber = nextOrderNumber.toString().padStart(6, "0");
 
   // Combine the prefix and padded numeric portion to create the new order ID
-  const newOrderId = `RX:${paddedOrderNumber}`;
-
-  // Save the new order ID to the database
-  // await saveOrderIdToDatabase(newOrderId);
+  const newOrderId = `${lastOrderPrefix}:${paddedOrderNumber}`;
 
   // Return the new order ID
   return newOrderId;
 }
 
-// Helper function to generate a new prefix when both RX and RX1 are about to exceed their limits
-async function generateNewPrefix() {
-  // Retrieve the last order ID from the database or set it to RX1:000000 if no previous orders exist
-  const lastOrderId = (await getLastOrderIdFromDatabase()) || "RX1:000000";
+async function generateNewPrefix(lastOrderPrefix: string) {
+  let orderIdPrefix: string;
 
-  // Extract the prefix and numeric portion of the last order ID
-  const lastOrderPrefix = lastOrderId.substring(0, 3);
-  const lastOrderNumber = parseInt(lastOrderId.substring(3));
+  if (lastOrderPrefix === "RX:") {
+    // Retrieve the last order ID from the database or set it to RX1:000000 if no previous orders exist
+    const lastOrderId = (await getLastOrderIdFromDatabase()) || "RX1:000000";
 
-  // Check if the numeric portion of the order ID is about to exceed six digits
-  if (lastOrderNumber >= 999999) {
+    // Extract the prefix and numeric portion of the last order ID
+    orderIdPrefix = lastOrderId.split(":")[0];
+    const lastOrderNumber = parseInt(lastOrderId.split(":")[1]);
+
+    // Check if the numeric portion of the order ID is about to exceed six digits
+    if (lastOrderNumber >= 999999) {
+      // Increment the prefix to the next number
+      return "RX1";
+    }
+
+    // Pad the numeric portion with leading zeroes to create a 6-digit number
+    const paddedOrderNumber = (lastOrderNumber + 1).toString().padStart(6, "0");
+
+    // Return the original prefix concatenated with the padded numeric portion
+    return orderIdPrefix.replace("RX", "RX1") + paddedOrderNumber;
+  } else {
+    // Extract the numeric portion of the last order ID
+
+    let rxNumber = lastOrderPrefix?.match(/RX(\d+)/)?.[1];
+    let lastOrderNumber = rxNumber ? parseInt(rxNumber) : 0;
+
+    // Check if the numeric portion of the order ID is about to exceed six digits
+    if (lastOrderNumber >= 99) {
+      throw new Error("Cannot generate new order ID: maximum prefix exceeded");
+    }
+
     // Increment the prefix to the next number
-    const nextPrefixNumber = parseInt(lastOrderPrefix.substring(2)) + 1;
-    const nextPrefix = `RX${nextPrefixNumber}`;
-    return nextPrefix;
+    const nextPrefixNumber = lastOrderNumber + 1;
+    return `RX${nextPrefixNumber}`;
   }
-
-  // Pad the numeric portion with leading zeroes to create a 6-digit number
-  const paddedOrderNumber = (lastOrderNumber + 1).toString().padStart(6, "0");
-
-  // Return the original prefix concatenated with the padded numeric portion
-  return lastOrderPrefix.replace("RX", "RX1") + paddedOrderNumber;
 }
 
 // Helper function to retrieve the last order ID from the database
 async function getLastOrderIdFromDatabase() {
   try {
-    // Find the most recent order by sorting the orders collection by ID in descending order and selecting the first document
-    const mostRecentOrder = await Order.findOne()
-      .sort({ _id: -1 })
-      .select("orderId");
-
-    // If there are no orders in the database, return null
-    if (!mostRecentOrder) {
-      return null;
-    }
-
-    // Extract the order number from the ID and return the formatted order ID
-    const orderNumber = parseInt(
-      mostRecentOrder.orderId.toString().substring(3)
-    );
-
-    return `RX:${orderNumber.toString().padStart(6, "0")}`;
+    const mostRecentOrder = await Order.findOne().sort({ _id: -1 });
+    return mostRecentOrder ? mostRecentOrder.orderId : null;
   } catch (error) {
-    console.error("Error retrieving last order ID from database:", error);
-    throw error;
+    console.error(error);
+    throw new Error("Error retrieving last order ID from database");
   }
 }
