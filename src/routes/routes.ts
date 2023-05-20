afc476ce6381d09b5160efb01c119fff0f8fbc5c
Yusuf <yumustyologt@example.com>
2023-05-20 18:05:25 +0100

    updates

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -48,7 +48,7 @@ import {
   getCareerOpenings,
 } from "../controllers/";
 import { validateLoginParams } from "../middleware/login.middleware";
-import { checkAdminRole } from "../middleware/roleCheck.middleware";
+import { checkAdminRole, checkRole } from "../middleware/roleCheck.middleware";
 import { validateSignupParams } from "../middleware/signup.middleware";
 import { multerUpload } from "../middleware/multer.middleware";
 
@@ -69,43 +69,63 @@ router.post(
   addMedicationController
 );
 router.put("/update_medication", checkAdminRole, editMedicationController);
-router.post("/topup_wallet", topUpWalletController);
-router.post("/gift_wallet_topup", giftWalletTopUpController);
-router.post("/new_order", multerUpload.single("image"), addOrder);
-router.get("/get_order_by_id", getOrderById);
-router.get("/get_orders", getOrders);
-router.get("/get_user_orders", getUserOrders);
+router.post("/topup_wallet", checkRole, topUpWalletController);
+router.post("/gift_wallet_topup", checkRole, giftWalletTopUpController);
+router.post("/new_order", checkRole, multerUpload.single("image"), addOrder);
+router.get("/get_order_by_id", checkRole, getOrderById);
+router.get("/get_orders", checkRole, getOrders);
+router.get("/get_user_orders", checkRole, getUserOrders);
 router.put("/update_order_status", checkAdminRole, updateOrderStatus);
-router.post("/add_prescription", multerUpload.single("image"), addPrescription);
-router.get("/get_prescription_by_id", getPrescriptionById);
-router.get("/get_user_prescriptions", getUserPrescription);
-router.post("/delete_user_prescription", deleteUserPrescriptionById);
-router.get("/get_all_prescriptions", getPrescriptions);
-router.get("/get_user_transactions", getUserTransactions);
+router.post(
+  "/send_prescription",
+  checkRole,
+  multerUpload.single("image"),
+  addPrescription
+);
+router.get("/get_prescription_by_id", checkRole, getPrescriptionById);
+router.get("/get_user_prescriptions", checkRole, getUserPrescription);
+router.post("/delete_user_prescription", checkRole, deleteUserPrescriptionById);
+router.get("/get_all_prescriptions", checkRole, getPrescriptions);
+router.get("/get_user_transactions", checkRole, getUserTransactions);
 router.post("/otp/send_password_recovery_email", emailOtpRequestController);
 router.post("/otp/send_password_recovery_sms", smsOtpRequestController);
 router.post("/otp/verify_password_recovery_otp", smsOtpRequestController);
 router.post("/otp/update_password", updatePasswordController);
 router.post("/send_sms", sendSmsController);
-router.post("/add_family", addFamilyController);
-router.post("/get_user_family", getUserFamilyController);
-router.post("/delete_user_family", deleteUserFamilyController);
-router.post("/add_hmo", multerUpload.single("image"), addHmoController);
-router.post("/get_hmo_by_id", getHMOByIdController);
-router.post("/get_user_hmo", getUserHMOsController);
-router.post("/get_all_hmo", getAllHMOController);
-router.post("/delete_user_hmo", deleteUserHMOByIdController);
-router.post("/complete_order", uncompletedOrdersControllers);
-router.post("/add_shipping_address", addShippingAddressController);
-router.post("/get_user_shipping_address", getUserShippingAddressController);
+router.post("/add_family", checkRole, addFamilyController);
+router.post("/get_user_family", checkRole, getUserFamilyController);
+router.post("/delete_user_family", checkRole, deleteUserFamilyController);
+router.post(
+  "/add_hmo",
+  checkRole,
+  multerUpload.single("image"),
+  addHmoController
+);
+router.post("/get_hmo_by_id", checkRole, getHMOByIdController);
+router.post("/get_user_hmo", checkRole, getUserHMOsController);
+router.post("/get_all_hmo", checkRole, getAllHMOController);
+router.post("/delete_user_hmo", checkRole, deleteUserHMOByIdController);
+router.post("/complete_order", checkRole, uncompletedOrdersControllers);
+router.post("/add_shipping_address", checkRole, addShippingAddressController);
+router.post(
+  "/get_user_shipping_address",
+  checkRole,
+  getUserShippingAddressController
+);
 router.post(
   "/get_user_shipping_address_by_id",
+  checkRole,
   getUserShippingAddressByIdController
 );
 // router.get("/get_chats", getChatsController);
 // // router.post('/add_chats', addChatsController)
-router.get("get_users", getUsersController);
-router.post("/add_career_openings", checkAdminRole, createCareerOpening);
-router.get("/get_career_openings", getCareerOpenings);
+router.get("get_users", checkRole, getUsersController);
+router.post(
+  "/add_career_openings",
+  checkAdminRole,
+  checkAdminRole,
+  createCareerOpening
+);
+router.get("/get_career_openings", checkRole, getCareerOpenings);
 
 export default router;
