68ed0a816d351f240479d9bd53335509080c3d0e
Yusuf <yumustyologt@example.com>
2023-06-10 23:13:49 +0100

    updates

--- src/controllers/refreshToken.controller.ts
+++ src/controllers/refreshToken.controller.ts
@@ -4,7 +4,7 @@ import UserModel from "../models/User.model";
 import { JwtPayload, CustomRequest } from "../interface/generalInterface";
 
 // refresh token verification route controller
-const refreshTokenVerification = async (
+export const refreshTokenVerification = async (
   req: CustomRequest,
   res: Response,
   next: NextFunction
@@ -43,4 +43,4 @@ const refreshTokenVerification = async (
   });
 };
 
-export default refreshTokenVerification;
+// export default refreshTokenVerification;
