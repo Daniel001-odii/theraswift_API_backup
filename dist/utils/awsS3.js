03a2ae96eb3b8fc25e5e1f1853c2120ac7108f80
Yusuf <yumustyologt@example.com>
2023-06-17 02:04:55 +0100

    updates

--- dist/utils/awsS3.js
+++ dist/utils/awsS3.js
@@ -11,17 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uploadToS3 = void 0;
 const client_s3_1 = require("@aws-sdk/client-s3");
-const s3 = new client_s3_1.S3Client({
-    region: 'us-east-1',
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-});
 const uploadToS3 = (buffer, filename) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(process.env.AWS_ACCESS_KEY_ID);
-    console.log(process.env.AWS_SECRET_ACCESS_KEY);
-    console.log(process.env.AWS_BUCKET_NAME);
+    if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_BUCKET_NAME) {
+        return;
+    }
+    const s3 = new client_s3_1.S3Client({
+        region: 'us-east-1',
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+    });
     const params = {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: filename,
--- src/utils/awsS3.ts
+++ src/utils/awsS3.ts
@@ -1,20 +1,22 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-const s3 = new S3Client({
-  region: 'us-east-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
 
 export const uploadToS3 = async (buffer: Buffer, filename: string): Promise<any> => {
-  console.log(process.env.AWS_ACCESS_KEY_ID);
-  console.log(process.env.AWS_SECRET_ACCESS_KEY);
-  console.log(process.env.AWS_BUCKET_NAME);
+
+  if(!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_BUCKET_NAME){
+    return
+  }
+
+  const s3 = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME!,
+    Bucket: process.env.AWS_BUCKET_NAME,
     Key: filename,
     Body: buffer,
     ContentType: 'image/jpeg',
