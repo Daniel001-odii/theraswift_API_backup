8dd4064b2a079ab47648607e85d10ae27b2478fa
Odii Daniel <xenithheight@gmail.com>
2024-10-13 21:53:18 +0100

    first dangerous commit

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,21 @@
+import * as admin from 'firebase-admin'
+
+
+interface FirebaseConfig {
+    projectId: string;
+    privateKey: string;
+    clientEmail: string;
+}
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID as string,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+    } as FirebaseConfig),
+    storageBucket: `gs://${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+});
+
+const bucket = admin.storage().bucket();
+
+export { bucket };
---
+++ src/utils/firebase.upload.utility.ts
@@ -0,0 +1,21 @@
+import { bucket } from "../config/firebase.config";
+
+export const uploadHMOImages = async (file:any) => {
+    try{
+        const file_path = file.filepath;
+        const remote_file_path = `HMO_images/${file.originalFilename}`;
+
+        await bucket.upload(file_path, { destination: remote_file_path });
+        const options = {
+            action: 'read',
+            expires: '01-01-2100'
+        };
+
+        const signed_url: any = await bucket.file(remote_file_path).getSignedUrl(options);
+        const image_url = signed_url[0];
+
+        return { success: true, url:image_url};
+    }catch(error:any){
+        return { success: false, error: error.message };
+    }
+}
